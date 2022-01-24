8d26446af057903df45c388975b64f7746ef3684
LORDSER <88093029+Lord-official@users.noreply.github.com>
2022-01-24 15:16:07 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = function (m) {
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 let fs = require('fs')
 let name = this.contacts[m.sender].notify
-let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '9197783838987@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:ʟᴏғᴄ\nEND:VCARD`}}}
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   
   
@@ -12,7 +12,7 @@ let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?
   for (let i of global.owner.map(v => v + '@s.whatsapp.net'))
   for (let i of global.fake.map(v => v + '@s.whatsapp.net'))
   for (let i of global.fakeNum.map(v => v + '@s.whatsapp.net'))
-  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:</ʀᴇʟʟ.xls⁴̅⁰͍³〆>\nORG: Busy!!!\nitem2.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem2.X-ABLabel: Just random people\nEND:VCARD` })
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:</ʟᴏʀᴅ.oғC⁴̅⁰͍⁴〆>\nORG: Busy!!!\nitem2.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem2.X-ABLabel: HeHe\nEND:VCARD` })
   this.sendMessage(m.chat, { displayName: listOwner.length + 'hii', contacts: listOwner }, 'contactsArrayMessage', { quoted: fkon, contextInfo:{"forwardingScore": 1000, "isForwarded": true}})
 }
 handler.help = ['owner', 'creator']
