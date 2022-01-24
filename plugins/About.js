6684b2067c113b962f3639f8b739b82657cdedb0
LORDSER <88093029+Lord-official@users.noreply.github.com>
2022-01-24 15:18:48 +0530

    Update About.js

--- plugins/About.js
+++ plugins/About.js
@@ -2,7 +2,7 @@ let handler  = async (m, { itsu, usedPrefix: _p }) => {
       
 let arr = []
 for (let i = 0; i < 404; i++) arr.push({ productId: '4072560079514110' }) 
-let list = await itsu.prepareMessageFromContent(m.chat, { listMessage: { title: 'ꜱɪᴍᴩʟᴇ ᴡʜᴀᴛꜱᴀᴩᴩ ʙᴏᴛ',  description: `- _*About me*_\n\n- Owner : Rlxfly\n- Age: 15/ 9th grade class\n - Github: Rlxfly\n\n thx.` , listType: 2, productListInfo: { productSections: [{ title: 'github: Rlxfly', products: arr }], headerImage: { productId: '4072560079514110', jpegThumbnail: thumb3 }, businessOwnerJid: '6283820073017@s.whatsapp.net' }, footerText: `© ${itsu.getName('6283820073017@s.whatsapp.net')}` }}, { quoted: m })
+let list = await itsu.prepareMessageFromContent(m.chat, { listMessage: { title: 'ᴡʜᴀᴛꜱᴀᴩᴩ ʙᴏᴛ',  description: `- _*About me*_\n\n- Owner : LoRD\n- Age: 16/ 9th class\n - Github: Lord-official\n\n thx.` , listType: 2, productListInfo: { productSections: [{ title: 'github: Lord-official', products: arr }], headerImage: { productId: '4072560079514110', jpegThumbnail: thumb3 }, businessOwnerJid: '919778383987@s.whatsapp.net' }, footerText: `© ${itsu.getName('919778383987@s.whatsapp.net')}` }}, { quoted: m })
 itsu.relayWAMessage(list, { waitForAck: true })
 }
 
